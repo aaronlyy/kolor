6029f516389632d2482671f3f9c78f2322983f03
Aaron Levi Can <aaronlevican@gmail.com>
2024-02-28 17:40:42 +0100

    added more examples and a better description

--- examples/main.cpp
+++ examples/main.cpp
@@ -3,23 +3,40 @@
 
 int main() {
 
-    // set colors using constants
-    std::cout << kolor::ESCAPE << kolor::FOREGROUND << kolor::format_rgb(200, 20, 50) << kolor::M << "Change colors using constants\n" << kolor::RESET;
+    /**
+     * This is an example on how to use kolor.
+     * There are four different methods of changing the appearance of terminal output.
+     * 1. Inline constants
+     * 2. Inline set functions
+     * 3. Wrapper functions
+     * 4. Direct print functions
+     */
 
-    // set colors using inline set functions
-    std::cout << kolor::set_fg(100, 23, 66) << "Change color using inline set functions\n" << kolor::RESET;
+    // Option 1: Change appearance using inline constants
+    // 1.1 Turn text bold using the most verbose constants
+    std::cout << kolor::ESCAPE << kolor::BOLD << kolor::M << "This text was made bold using inline constants.\n" << kolor::ESCAPE << kolor::RESET << kolor::M;
 
-    // use print_bg function to change the background
-    kolor::print_bg("Change background using print_bg\n", 23, 123, 90);
+    // 1.2 Turn text bold using already escaped constants
+    std::cout << kolor::BOLD_E << "This text was also made bold but with less code.\n" << kolor::RESET_E;
 
-    // use print_fg function to change the foreground
-    kolor::print_fg("Change foreground using print_fg\n", 23, 123, 90);
+    // Options 2: Change appearance using set functions
+    // 2.1 Turn text italic using the set_italic and set_reset
+    std::cout << kolor::set_italic() << "This text is italic.\n" << kolor::set_reset();
 
-    // use print function to change background and foreground of an output
-    kolor::print("use print function to change background and foreground", 144, 252, 3, 3, 140, 252);
+    // 2.2 Turn text green using set_fg_rgb and set_reset
+    std::cout << kolor::set_fg_rgb(0,255,0) << "This text was made green using rgb values.\n" << kolor::set_reset();
 
-    // reset everything
-    std::cout << kolor::RESET;
+    // Option 3: Change appearance using wrapper functions
+    // wip
 
+    // Option 4: Directly print text using print functions
+    // 4.1 Print underlined text using print_underline
+    kolor::print_underline("This text is underlined.\n");
+
+    // 4.2 Print red text using print_fg_rgb
+    kolor::print_fg_rgb("This text is red.\n", 255, 0, 0);
+
+    // 4.3 Print text with blue background using print_bg_rgb
+    kolor::print_bg_rgb("This text has a blue background.\n", 0, 0, 255);
     return 0;
 }
